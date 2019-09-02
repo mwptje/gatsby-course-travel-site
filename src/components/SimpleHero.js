a54aeb462172e664abe6a4b7ff4019c66d337ab8
mvdvalk <46578571+mwptje@users.noreply.github.com>
2019-09-02 15:13:03 +0200

    front page hero and banner added

---
+++ src/components/SimpleHero.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+// Note background images is from the
+// layout.css
+const SimpleHero = ({ children }) => {
+  return <header className="defaultHero">{children}</header>
+}
+
+export default SimpleHero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,26 @@
 import React from "react"
-
+import { Link } from "gatsby"
 // Encompassing layout
 import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+
 // content between the Layout tags is know as children
 // and passed to a sush to the layout component
 // Import icon from fontawsom using react-icons
 // import { FaBeer } from "react-icons/fa"
 export default () => (
   <Layout>
-    <div>Hello from backroads</div>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, atque?"
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
+    >
   </Layout>
 )
