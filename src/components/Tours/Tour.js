0e55424fe8426208699c6eec773e58b8e9bc4119
mvdvalk <46578571+mwptje@users.noreply.github.com>
2019-09-10 10:56:22 +0200

    adding gatsby.nodes.js

--- src/components/Tours/Tour.js
+++ src/components/Tours/Tour.js
@@ -3,11 +3,29 @@ import Image from "gatsby-image"
 import styles from "../../css/tour.module.css"
 import { Link } from "gatsby"
 import { FaMap } from "react-icons/fa"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+// Get a default image just in case no images where found
+const getImage = graphql`
+  query {
+    file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Tour = ({ tour }) => {
+  const data = useStaticQuery(getImage)
   // do some destructuring using curly braces
   const { name, price, country, days, slug, images } = tour
-  let mainImage = images[0].fluid
+  const img = data.file.childSharpImage
+  // In case no images have been found then use
+  // the default image
+  let mainImage = images ? images[0].fluid : img
 
   return (
     <article className={styles.tour}>
@@ -22,7 +40,8 @@ const Tour = ({ tour }) => {
         <div className={styles.info}>
           <h4 className={styles.country}>
             <FaMap className={styles.icon} />
-            {country}
+            {/* Note using a default when null value  */}
+            {country || "default country"}
           </h4>
           <div className={styles.details}>
             <h6>{days} days</h6>
@@ -34,4 +53,17 @@ const Tour = ({ tour }) => {
   )
 }
 
+// Determine if a simple property is empty so
+// as to fix with a default value
+// As is it will give a warning in the console log
+// so you can detect missing data
+Tour.propTypes = {
+  tour: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    country: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    days: PropTypes.number.isRequired,
+    images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }),
+}
 export default Tour
