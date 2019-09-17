e8d4f7899d25dd5753b67eba907d0d0cebb59585
mvdvalk <46578571+mwptje@users.noreply.github.com>
2019-09-17 13:28:17 +0200

    up to rendering posts

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,7 +1,80 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import styles from "../css/single-blog.module.css"
+import { Link } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { node } from "prop-types"
 
-const BlogTemplate = () => {
-  return <div>this is a single blog page</div>
+const Blog = ({ data }) => {
+  const {
+    title,
+    published,
+    text: { json },
+  } = data.post
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <div className="rich">
+            <h3>This is awesome</h3>
+            <img width="400" src={node.data.target.fields.file["en-US"].url} />
+            <p>images provided by john doe</p>
+          </div>
+        )
+      },
+      "embedded-entry-block": node => {
+        const { title, image, text } = node.data.target.fields
+        return (
+          <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <h1>This is another post: {title["en-US"]}</h1>
+            <img
+              width="400"
+              src={image["en-US"].fields.file["en-US"].url}
+              alt="single embedded post image"
+            />
+            {documentToReactComponents(text["en-US"])}
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+        )
+      },
+    },
+  }
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <div className={styles.center}>
+          <h1>{title}</h1>
+          <h4>published at: {published}</h4>
+          <article className={styles.post}>
+            {documentToReactComponents(json, options)}
+          </article>
+          <Link to="/blog" className="btn-primary">
+            all posts
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
-export default BlogTemplate
+export const query = graphql`
+  query getPost($slug: String!) {
+    post: contentfulPost(slug: { eq: $slug }) {
+      title
+      published(formatString: "MMMM Do YYYY")
+      text {
+        json
+      }
+    }
+  }
+`
+
+export default Blog
