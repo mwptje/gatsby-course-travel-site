a6e914a2a7d78498a3c8c699977de3cf90d6b888
mvdvalk <46578571+mwptje@users.noreply.github.com>
2019-09-18 10:33:42 +0200

    finished blogs section

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,4 +39,22 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     })
   })
+  // amount of posts
+  const posts = data.posts.edges
+  // how many posts per page
+  const postsPerPage = 5
+  // how many pages
+  const numPages = Math.ceil(posts.length / postsPerPage)
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blogs` : `/blogs/${i + 1}`,
+      component: path.resolve("./src/templates/blog-list-template.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
+  })
 }
---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,82 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
+import styles from "../css/blog.module.css"
+import BlogCard from "../components/Blog/BlogCard"
+import Title from "../components/Title"
+
+const Blog = props => {
+  const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage =
+    currentPage - 1 === 1 ? `/blogs` : `/blogs/${currentPage - 1}`
+  const nextPage = `/blogs/${currentPage + 1}`
+  const { data } = props
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <Title title="latest" subtitle="posts" />
+        <div className={styles.center}>
+          {data.posts.edges.map(({ node }) => {
+            return <BlogCard key={node.id} blog={node} />
+          })}
+        </div>
+        <section className={styles.links}>
+          {!isFirst && (
+            <Link to={prevPage} className={styles.link}>
+              Prev
+            </Link>
+          )}
+
+          {Array.from({ length: numPages }, (_, i) => {
+            return (
+              <Link
+                key={i}
+                to={`/blogs/${i === 0 ? "" : i + 1}`}
+                className={
+                  i + 1 === currentPage
+                    ? `${styles.link} ${styles.active}`
+                    : `${styles.link}`
+                }
+              >
+                {i + 1}
+              </Link>
+            )
+          })}
+          {!isLast && (
+            <Link to={nextPage} className={styles.link}>
+              Next
+            </Link>
+          )}
+        </section>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(
+      limit: $limit
+      skip: $skip
+      sort: { fields: published, order: DESC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do YYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default Blog
